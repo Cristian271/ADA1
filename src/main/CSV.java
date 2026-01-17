72108084e9a0d1b6ad3cea6d030216d3ad14eed7
AnelisseRivera <anelisseriveramanzanero@MacBook-Air-de-Anelisse-2.local>
2026-01-17 17:19:35 -0600

    Registro de calificaciones y generacion de archivo

--- src/main/CSV.java
+++ src/main/CSV.java
@@ -1,36 +1,52 @@
 package main;
 import javax.swing.*;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CSV {
     private BufferedReader lector; //Lee el archivo
     private String linea; // recibe la linea de cada fila
     private String partes[]; // almacena cada linea leida
     public static final String RUTA = "src/main/ListaDeAlumnos.csv";
+    public static final String RUTA_DEST = "src/main/Reporte_Califaciones.csv";
 
 
 
-    public void read() throws FileNotFoundException {
+    public List<Estudiante> read() throws FileNotFoundException {
+        List<Estudiante> lista = new ArrayList<>();
       try{
           lector = new BufferedReader(new FileReader(RUTA));
           while((linea = lector.readLine()) != null){
               partes = linea.split(",");
-              print();
-              System.out.println();
+              if (partes.length >= 4) {
+                  // Creamos el objeto de estudiante con los datos del cdv
+                  Estudiante e = new Estudiante(partes[0], partes[1], partes[2], partes[3]); lista.add(e);
+              }
+
           }
 
           lector.close();
       } catch (Exception e){
           JOptionPane.showMessageDialog(null, e);
       }
+      return lista;
     }
 
     public void print(){
         for(int i = 0; i < partes.length; i++) System.out.println(partes[i] + "  |  ");
     }
 
+    public void generarReporte(List<Estudiante> lista) {
+        try (PrintWriter archivoSalida = new PrintWriter(new FileWriter(RUTA_DEST))) {
+            for (Estudiante e : lista) {
+                archivoSalida.println(e.getMatricula() + ",Diseño de Software," + e.getGrade());
+            }
+            JOptionPane.showMessageDialog(null, "Archivo generado con éxito.");
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "Error al guardar: " + e.getMessage());
+        }
+    }
 
 
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,12 +1,42 @@
 package main;
 
 import java.io.FileNotFoundException;
-
+import java.util.*;
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         CSV csv = new CSV();
-        csv.read();
+        List<Estudiante> alumnos = csv.read();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("REGISTRO DE CALIFICACIONES FINALES");
 
+        for (Estudiante alumno : alumnos) {
+            int grade = 0;
+            while (grade < 1 || grade > 100) {
+                System.out.print("Calificación de " + alumno.getFullName() + ": ");
+                try {
+                    grade = Integer.parseInt(scanner.nextLine());
+                    if (grade < 1 || grade > 100) {
+                        System.out.println("La calificacion debe ser valida (1-100).");
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("El dato debe ser un número entero.");
+                }
+            }
+            alumno.setGrade(grade);
+        }
+        // Esta verificacion segun yo no es necesaria hasta ahora pero veo que esta como un punto de la tarea entonces no se
+        boolean allGraded = true;
+        for (Estudiante alumno : alumnos) {
+            if (alumno.getGrade() == 0) {
+                allGraded = false;
+                break;
+            }
+        }
+        if (allGraded) {
+            csv.generarReporte(alumnos);
+        } else {
+            System.out.println("Faltan alumnos por calificar");
+        }
     }
 
 
