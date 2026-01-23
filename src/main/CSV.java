e3a8dc226ff6d245a031b8fae7698ca89e36886c
Cristian Sanchez <cristianss219@gmail.com>
2026-01-22 19:26:07 -0600

    login

--- src/main/CSV.java
+++ src/main/CSV.java
@@ -1,42 +1,45 @@
 package main;
+
 import javax.swing.*;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class CSV {
-    private BufferedReader lector; //Lee el archivo
+    private BufferedReader lector; // Lee el archivo
     private String linea; // recibe la linea de cada fila
     private String partes[]; // almacena cada linea leida
-    public static final String RUTA = "src/main/ListaDeAlumnos.csv";
-    public static final String RUTA_DEST = "src/main/Reporte_Califaciones.csv";
+    public static final String RUTA = "src/main/ListaDeAlumnos.csv"; // ruta física
+    public static final String RUTA_DEST = "out/Reporte_Califaciones.csv";
+    public static final String RUTA_USR = "src/main/ListaDeUsuarios.csv"; // ruta física
 
-
-
+    // Leer estudiantes
     public List<Estudiante> read() throws FileNotFoundException {
         List<Estudiante> lista = new ArrayList<>();
-      try{
-          lector = new BufferedReader(new FileReader(RUTA));
-          while((linea = lector.readLine()) != null){
-              partes = linea.split(",");
-              if (partes.length >= 4) {
-                  // Creamos el objeto de estudiante con los datos del cdv
-                  Estudiante e = new Estudiante(partes[0], partes[1], partes[2], partes[3]); lista.add(e);
-              }
-
-          }
-
-          lector.close();
-      } catch (Exception e){
-          JOptionPane.showMessageDialog(null, e);
-      }
-      return lista;
+        try {
+            lector = new BufferedReader(new FileReader(RUTA));
+            while ((linea = lector.readLine()) != null) {
+                partes = linea.split(",");
+                if (partes.length >= 4) {
+                    Estudiante e = new Estudiante(partes[0], partes[1], partes[2], partes[3]);
+                    lista.add(e);
+                }
+            }
+            lector.close();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+        }
+        return lista;
     }
 
-    public void print(){
-        for(int i = 0; i < partes.length; i++) System.out.println(partes[i] + "  |  ");
+    // Imprimir la última línea leída (útil para debug)
+    public void print() {
+        for (int i = 0; i < partes.length; i++)
+            System.out.println(partes[i] + "  |  ");
     }
 
+    // Generar reporte
     public void generarReporte(List<Estudiante> lista) {
         try (PrintWriter archivoSalida = new PrintWriter(new FileWriter(RUTA_DEST))) {
             for (Estudiante e : lista) {
@@ -48,5 +51,34 @@ public class CSV {
         }
     }
 
+    // Validar usuario y contraseña
+    public boolean validar(String usuario, String contrasena) {
+        String con = "";
+        for (int i = 0; i < contrasena.length(); i++) {
+            char a = (char) (contrasena.charAt(i) + 2); // Encriptación simple
+            con += a;
+        }
+        //System.out.println("Resultado de encriptamiento: " + con); // Para debug
 
+        try {
+            lector = new BufferedReader(new FileReader(RUTA_USR));
+            while ((linea = lector.readLine()) != null) {
+                partes = linea.split(",");
+                if (partes.length >= 2) {
+                    if (Objects.equals(partes[0], usuario) && Objects.equals(partes[1], con)) {
+                        lector.close();
+                        return true;
+                    }
+                }
+            }
+            lector.close();
+        } catch (FileNotFoundException e) {
+            JOptionPane.showMessageDialog(null, "No se encontró " + RUTA_USR);
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+
+        return false;
+    }
 }
+
--- src/main/Main.java
+++ src/main/Main.java
@@ -4,38 +4,104 @@ import java.io.FileNotFoundException;
 import java.util.*;
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        CSV csv = new CSV();
-        List<Estudiante> alumnos = csv.read();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("REGISTRO DE CALIFICACIONES FINALES");
+        CSV csv = new CSV();
+        String usuario, contrasena;
+        System.out.println("----------------");
+        System.out.println("INICIO DE SESIÓN");
+        System.out.println("----------------");
 
-        for (Estudiante alumno : alumnos) {
-            int grade = 0;
-            while (grade < 1 || grade > 100) {
-                System.out.print("Calificación de " + alumno.getFullName() + ": ");
-                try {
-                    grade = Integer.parseInt(scanner.nextLine());
-                    if (grade < 1 || grade > 100) {
-                        System.out.println("La calificacion debe ser valida (1-100).");
-                    }
-                } catch (NumberFormatException e) {
-                    System.out.println("El dato debe ser un número entero.");
+        System.out.println("INGRESE EL USUARIO: ");
+        usuario = scanner.nextLine();
+        System.out.println("INGRESE LA CONTRASEÑA: ");
+        contrasena = scanner.nextLine();
+        if(!csv.validar(usuario, contrasena)){
+            System.exit(0);
+        }
+
+        System.out.println("----------------------------------");
+        System.out.println("REGISTRO DE CALIFICACIONES FINALES");
+        System.out.println("----------------------------------");
+        int op = -1;
+        boolean escaneable = true; // validaciones, para saber si ya seleccionó una opción antes
+        List<Estudiante> alumnos = csv.read();
+        while (op != 4) {
+            if(escaneable){
+                System.out.println("1) INGRESAR CALIFACIONES");
+            }
+
+            if(!escaneable){
+                System.out.println("2) GENERAR REPORTE DE CALIFICACIONES");
+            }
+
+            if(!escaneable) {
+                System.out.println("3) GENERAR PDF (TODAVIA NO FUNCIONAL)");
+            }
+            System.out.println("4) SALIR");
+            op = scanner.nextInt();
+            if(op == 1){
+                if(!escaneable){
+                    System.out.println("No valido");
+                    continue;
                 }
             }
-            alumno.setGrade(grade);
-        }
-        // Esta verificacion segun yo no es necesaria hasta ahora pero veo que esta como un punto de la tarea entonces no se
-        boolean allGraded = true;
-        for (Estudiante alumno : alumnos) {
-            if (alumno.getGrade() == 0) {
-                allGraded = false;
-                break;
+
+            else if(op == 2){
+                if(escaneable){
+                System.out.println("No valido");
+                    continue;
+                }
+
             }
-        }
-        if (allGraded) {
-            csv.generarReporte(alumnos);
-        } else {
-            System.out.println("Faltan alumnos por calificar");
+
+            if(op >= 4){
+                System.exit(0);
+            }
+
+            switch (op) {
+                case 1:
+                    escaneable = false;
+                    for (Estudiante alumno : alumnos) {
+                        int grade = 0;
+                        while (grade < 1 || grade > 100) {
+                            System.out.print("Calificación de " + alumno.getFullName() + ": ");
+                            try {
+                                //grade = Integer.parseInt(scanner.nextLine());
+                                grade = scanner.nextInt();
+                                if (grade < 1 || grade > 100) {
+                                    System.out.println("La calificacion debe ser valida (1-100).");
+                                }
+                            } catch (NumberFormatException e) {
+                                System.out.println("El dato debe ser un número entero.");
+                            }
+                        }
+                        alumno.setGrade(grade);
+                    }
+                    break;
+                case 2:
+                    boolean allGraded = true;
+                    for (Estudiante alumno : alumnos) {
+                        if (alumno.getGrade() == 0) {
+                            allGraded = false;
+                            break;
+                        }
+                    }
+                    if (allGraded) {
+                        csv.generarReporte(alumnos);
+                    } else {
+                        System.out.println("Faltan alumnos por calificar");
+                    }
+                    break;
+                case 3:
+                    System.out.println("nada");
+                    break;
+                default:
+                    break;
+            }
+
+
+
+
         }
     }
 
